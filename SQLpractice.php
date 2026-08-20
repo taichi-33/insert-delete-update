73fcbbe69673ad8599627acdd2ea58743740dfb9
taichi-33 <taichi_itayama@outlook.jp>
2026-08-20 13:16:18 +0900

    Add files via upload

--- SQLpractice.php
+++ SQLpractice.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>タイトル</title>
-    <link rel="stylesheet" href="cssを指定">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-
+    
     <?php
 
     $pdo = new PDO('mysql:host=localhost;port=3306;dbname=todo','root','taichi.0315');
@@ -21,10 +21,32 @@
             $stmt -> execute([':title' => $title]);
         }
         header('Location: ' .$_SERVER['PHP_SELF']);
-            exit;
+        exit;
     }
 
-    //$delete = $_POST['delete_id'] ?? null ;
+    $check_id = $_POST['check_id'] ?? null ;
+
+    if(isset($_POST['check_id'])) {
+    $stmt = $pdo -> prepare("SELECT * FROM tasks WHERE id = :id");
+    $stmt -> execute([':id'  => $check_id]);
+    $target = $stmt -> fetch();
+    //var_dump($target);
+    $is_done = $target["is_done"];
+    //echo $is_done ;
+        if($is_done == 0) {
+            $stmt = $pdo -> prepare("UPDATE tasks SET is_done = 1 WHERE id = :id") ;
+            $stmt -> execute([':id' => $check_id]) ;
+            //return;
+        } else {
+            $stmt = $pdo -> prepare("UPDATE tasks SET is_done = 0 WHERE id = :id") ;
+            $stmt -> execute([':id' => $check_id]) ;
+            //return;
+        }
+        
+        header('Location: '.$_SERVER['PHP_SELF']);
+        exit;
+    }
+
 
     if (isset($_POST['delete_id'])) {
         $stmt = $pdo -> prepare("DELETE from tasks WHERE id = :id") ;
@@ -59,18 +81,24 @@
     <ul>
         <?php foreach ($tasks as $task): ?>
             <li>
-                <form action="" method="post">
-                    <?php echo htmlspecialchars($task['title'],ENT_QUOTES,'UTF-8'); ?>
-                    <input type="hidden" name="delete_id" value="<?php echo $task['id']; ?>">
-                    <input type="submit" value="削除">
+                <form class="form checkbox" action="" method="post">
+                    <input type="hidden" name="check_id" value="<?php echo $task['id']; ?>">
+                    <input type="checkbox" name="done" onchange="this.form.submit()" <?php echo $task['is_done']==1 ? 'checked' : '' ?>>
                 </form>
-                <form action="" method="post">
+                <form class="form title <?php echo $task['is_done']==1 ? 'completed' : 'orange' ?>" action="" method="post">
+                    <?php echo htmlspecialchars($task['title'],ENT_QUOTES,'UTF-8'); ?>
+                </form>
+                <form class="form edit" action="" method="post">
                     <input type="hidden" name="edit_id" value="<?php echo $task['id']; ?>">
                     <input type="text" name="edit">
                     <input type="submit" value="編集">
                 </form>
+                <form class="form delete" action="" method="post">
+                    <input type="hidden" name="delete_id" value="<?php echo $task['id']; ?>">
+                    <input type="submit" value="削除">
+                </form>
             </li>
         <?php endforeach; ?>
     </ul>
-    
+
 </body>
